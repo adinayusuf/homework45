ee32b0e9187a832585c78ffbcb817d7c122b7986
Adina Yusuf <adinkayus97@gmail.com>
2022-06-23 17:14:31 +0600

    Commit 5: add admin and timezone

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from webapp.models import To_do_list
+
+
+
+class ToDoListAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'date_of_completion']
+    list_filter = ['description']
+    list_display_links = ['description']
+    search_fields = ['description', 'status']
+    fields = ['description', 'status', 'text', 'date_of_completion']
+    readonly_fields = ['date_of_completion']
+
+
+
+admin.site.register(To_do_list,ToDoListAdmin)
