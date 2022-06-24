5510cfae6970bce2ac9226d8cd4a341bda75f4bc
Adina Yusuf <adinkayus97@gmail.com>
2022-06-24 15:26:34 +0600

    Commit 9: added delete func and refresh index and added url for delete

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from webapp.views import index_view, create_description, list_view
+from webapp.views import index_view, create_description, list_view, delete_description
 
 urlpatterns = [
     path('', index_view),
     path('to_do_lists/add/', create_description),
-    path('to_do_lists/', list_view)
+    path('to_do_lists/', list_view),
+    path('delete/<id>', delete_description)
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-from webapp.models import To_do_list
+from webapp.models import To_do_list, STATUS_CHOICES
 
 
 def index_view(request):
     to_do_list = To_do_list.objects.order_by('-date_of_completion')
     context = {'to_do_list': to_do_list}
     return render(request, 'index.html', context)
+
 
 def list_view(request):
     pk = request.GET.get("pk")
@@ -17,10 +18,16 @@ def list_view(request):
 
 def create_description(request):
     if request.method == "GET":
-        return render(request, "create.html")
+        return render(request, "create.html", {'status': STATUS_CHOICES})
     else:
         description = request.POST.get("description")
         status = request.POST.get("status")
         new_des = To_do_list.objects.create(description=description, status=status)
         context = {"to_do_list": new_des}
         return render(request, "ditail_view.html", context)
+
+
+def delete_description(request, id):
+    to_do_list = To_do_list.objects.get(pk=id)
+    to_do_list.delete()
+    return render(request, 'index.html')
