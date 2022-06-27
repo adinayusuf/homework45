53dcf18ea7bbc78e9f2ad399e43e1997ce07a3f4
Adina Yusuf <adinkayus97@gmail.com>
2022-06-27 11:02:26 +0600

    Commit 13: added bodstrap and  change models datafield to charfield

--- webapp/models.py
+++ webapp/models.py
@@ -7,7 +7,7 @@ STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),
 class To_do_list(models.Model):
     description = models.CharField(max_length=100, null=False, blank=False, verbose_name="Описание")
     status = models.CharField(max_length=40, choices=STATUS_CHOICES, null=False, blank=False, default=STATUS_CHOICES[0][0], verbose_name='Статус')
-    date_of_completion = models.DateField(verbose_name='Время выполнения')
+    date_of_completion = models.CharField(max_length=12, blank=True, null=True, verbose_name='Время выполнения')
 
     def __str__(self):
         return f"{self.id}. {self.description}: {self.status}"
