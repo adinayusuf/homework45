d3a4d569b4c945eaf0a5b788f4e5c9ab636fc7b0
Adina Yusuf <adinkayus97@gmail.com>
2022-06-28 13:27:51 +0600

    changes name of func create_description to create_task

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from webapp.views import index_view, create_description, list_view, delete_description
+from webapp.views import index_view, create_task, list_view, delete_description
 
 urlpatterns = [
     path('', index_view, name="index"),
-    path('to_do_lists/add/', create_description ,name ="create_description" ),
+    path('to_do_lists/add/', create_task,name ="create_task" ),
     path('to_do_lists/<int:pk>/', list_view, name ="list_view" ),
     path('delete/<int:pk>/', delete_description, name= "delete_description"),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect, HttpResponseNotFound
-# Create your views here.
-from django.urls import reverse
+from django.http import HttpResponseNotFound
+
 
 from webapp.models import To_do_list, STATUS_CHOICES
 
@@ -12,18 +11,15 @@ def index_view(request):
     return render(request, 'index.html', context)
 
 
-
 def list_view(request, pk):
-    # pk = request.GET.get("pk")
     try:
         to_do_list = To_do_list.objects.get(pk=pk)
     except To_do_list.DoesNotExist:
         return HttpResponseNotFound("Page not find")
     return render(request, "ditail_view.html", {'to_do_list': to_do_list})
-    # return redirect("ditail_view.html", pk=to_do_list.pk)
 
 
-def create_description(request):
+def create_task(request):
     if request.method == "GET":
         return render(request, "create.html", {'status': STATUS_CHOICES})
     else:
@@ -34,12 +30,10 @@ def create_description(request):
         new_des = To_do_list.objects.create(description=description, status=status,
                                             date_of_completion=date_of_completion, text=text)
         new_des.save()
-        # return HttpResponseRedirect(f"/to_do_lists/{new_des.pk}")
-        # return HttpResponseRedirect(reverse("list_view", kwargs={"pk": new_des.pk}))
         return redirect("list_view", pk=new_des.pk)
 
 
 def delete_description(request, pk):
     to_do_list = To_do_list.objects.get(pk=pk)
     to_do_list.delete()
-    return redirect("delete_description", pk=to_do_list.pk)
+    return redirect("index")
